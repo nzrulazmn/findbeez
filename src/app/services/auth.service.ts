07b05956f0af1377156cf59da3c95a674d50362e
Eiji <r-six@hotmail.com>
2023-09-27 19:46:09 +0800

    +function name

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,7 +14,7 @@ const auth = getAuth(app);
 export class AuthService {
 
   constructor() { }
-  
+
   // to be connected to the page service
 
   createUser (email: string, password: string, username: any) {
@@ -25,9 +25,9 @@ export class AuthService {
         updateProfile(user, {
           displayName: username, photoURL: "https://firebasestorage.googleapis.com/v0/b/findbeez.appspot.com/o/display-picture%2Fuser-default.png?alt=media&token=17253f11-b1c0-4e36-9f09-edc78076d7be"
         }).then(() => {
-          
+
         }).catch((error) => {
-          
+
         });
       })
     })
@@ -47,9 +47,25 @@ export class AuthService {
     })
   }
 
-}
-    
+  updateUsername (newName : string) {
+    const cUser = auth.currentUser;
+        return new Promise<any>((resolve,reject) => {
+          if (cUser != null) {
+            updateProfile(cUser, {
+              displayName: "test"
+            }).then(() => {
   
+            }).catch((error) => {
+  
+            })
+          }
+       });
+      
+  }
 
-  
+}
+
+
+
+
 
